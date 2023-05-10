9d0dadd1e0e5d1a3c33e7eedb7b5351d32fe5cf4
kell20 <kelvin.jose220@gmail.com>
2023-05-10 11:02:41 -0700

    multhreading simple program

--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
 public class Main{
     public static void main(String []args){
-        System.out.println("hello programming");
-        System.out.println("hello");
+       th thread1=new th();
+       thread1.tel.start();       
+    }
+}
+class th implements Runnable{
+    Thread tel;
+    th(){
+      tel=new Thread(this);
+    }
+
+   @Override
+    public void run(){
+        try{
+            Thread.sleep(7000);
+        }catch(InterruptedException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("hello..this is simple multthreading program");
     }
 }
